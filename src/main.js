f06614ad7feb412ebf840d5d1ad781541352604e
cuicui <2865653500@qq.com>
2019-08-12 15:06:55 +0800

    登录功能模块基础布局搭建好,,,vue_jobProject_pc_day01代码完毕

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import elementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router/router'
+import '@/style/global.css'
 
 // 此条语句,配置生产环境下尽量少提示日志???
 Vue.config.productionTip = false
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    This is login page ...
+    <el-card class="my-card"></el-card>
   </div>
 </template>
 
@@ -11,5 +11,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.container{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background:url("../assets/images/login_bg.jpg") no-repeat center / cover;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-60%)
+  }
+}
 </style>
