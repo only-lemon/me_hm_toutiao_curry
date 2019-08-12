a766c2b65fc73524249b03afb0059ecc08a9e672
cuicui <2865653500@qq.com>
2019-08-13 01:42:49 +0800

    登录功能完善能向后台发送登录成功的请求,并带回来token数据了,,接下来做首页相关的内容

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import elementUi from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router/router'
 import '@/style/global.css'
+import axios from 'axios'
+
+// 挂载在Vue原型链上,,,共享给所有Vue创建出来的对象,实例
+Vue.prototype.$cuicui = axios
 
 // 此条语句,配置生产环境下尽量少提示日志???
 Vue.config.productionTip = false
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,12 +1,77 @@
 <template>
   <div class="container">
-    <el-card class="my-card"></el-card>
+    <el-card class="my-card">
+      <img src="../assets/images/logo_index.png" alt />
+      <!-- 使用elementUI组件库,,框架的,,form组件 -->
+      <el-form :model="loginForm"
+               :rules="loginVerifyRules"
+               ref = "integralVerify"
+      >
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:236px;margin-right:10px"
+          ></el-input>
+          <el-button type="primary">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="bool">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="warning" style="width:100%" @click="login()">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    const checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) { return callback(new Error('手机格式不对啊!')) }
+      callback()
+    }
+    return {
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      bool: true,
+      loginVerifyRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位验证码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 别人组件的函数调用: 通过DOM对象去调用
+      // 对整个表单进行校验
+      this.$refs.integralVerify.validate((validated) => {
+        if (validated) {
+          console.log('登录表单数据全部通过验证!!!')
+          // 校验成功发起登录请求操作!!!
+          this.$cuicui.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm).then((result) => {
+            console.log(result)
+          }).catch(() => {
+            this.$message.info('登录失败 -----> 手机号或验证码错误!')
+          })
+        }
+      })
+    }
+  }
 }
 </script>
 
@@ -22,7 +87,13 @@ export default {
     position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-60%)
+    transform: translate(-50%,-60%);
+    img {
+      width: 200px;
+      display: block;
+      margin: 0 auto 30px;
+
+    }
   }
 }
 </style>
